3ba4ed7d1fdd75c7b06a88f7f3ba1eda4c54ea3e
smharini18 <smharini18@gmail.com>
2026-06-05 10:50:10 +0530

    Create C_Program_3.c

---
+++ C_Program_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    int i=0,sum=0;
+    loop: if(i<5)
+    {   
+        i=i+1;
+        sum=sum+i;
+        goto loop;
+    }
+     printf("%d",sum);
+    return 0;
+}
